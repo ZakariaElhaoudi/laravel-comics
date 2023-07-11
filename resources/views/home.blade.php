092952c2f206d3fdd4259313361def07d6259abc
Zakaria96 <zakarialavoro@gmail.com>
2023-07-11 17:36:12 +0200

    aggiunto artisti e scrittori

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,23 +8,34 @@
 
 @section('content')
 
-<h1>hello world</h1>
+<h1 class="text-center">Fumetti</h1>
 <a href="{{ route('main') }}">MAIN</a>
 <a href="{{ route('home') }}">HOME</a>
 
 
     <div class="row">
         @foreach ($fumetti as $fumetto)
-            <div class="col-4 card">
+            <div class="col-4 card my-4 px-2">
                 <div class="card-img">
                     <img src="{{ $fumetto['thumb'] }}" alt="">
                 </div>
                 <h3>Titolo: {{ $fumetto['title'] }}</h3>
                 <p>Descrizione: {{ $fumetto['description'] }}</p>
-                <p>Prezzo: {{ $fumetto['price'] }}</p>
-                <p>Serie: {{ $fumetto['series'] }}</p>
-                <h5>Artista: {{ $fumetto['artists'] }}</h5>
-                <h6>Scrittore: {{ $fumetto['writers'] }}</h6>
+                <h4>Prezzo: {{ $fumetto['price'] }}</h4>
+                <h4>Serie: {{ $fumetto['series'] }}</h4>
+                <h5>Artisti: </h5>
+                <ul>
+                    @foreach ($fumetto['artists'] as $artist)
+                        <li>{{ $artist }}</li>
+                    @endforeach
+                </ul>
+                <h5>Scrittori:</h5>
+                <ul>
+                    @foreach ($fumetto['writers'] as $writer)
+                        <li>{{ $writer }}</li>
+                    @endforeach
+                </ul>
+
             </div>
         @endforeach
     </div>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,5 +8,7 @@
 
 @section('content')
 <a href="{{ route('home') }}">HOME</a>
+
+<h1>MAIN</h1>
     
 @endsection
